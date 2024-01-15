7a5fbeb63954f2aa99c5b3754725242c65ef3902
roycoMedia <makinderidwan73@gmail.com>
2024-01-15 23:19:48 +0100

    first commits

--- src/components/dashboardContent/pie_chart.tsx
+++ src/components/dashboardContent/pie_chart.tsx
@@ -1,33 +1,22 @@
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { PieChart, Pie, Sector } from "recharts";
 
 
 export default function Example ({data}: any) {
 
 const renderActiveShape = (props: any) => {
-  const RADIAN = Math.PI / 180;
   const {
     cx,
     cy,
-    midAngle,
     innerRadius,
     outerRadius,
     startAngle,
     endAngle,
-    fill,
     payload,
-    percent,
-    value
+    
   } = props;
-  const sin = Math.sin(-RADIAN * midAngle);
-  const cos = Math.cos(-RADIAN * midAngle);
-  const sx = cx + (outerRadius + 10) * cos;
-  const sy = cy + (outerRadius + 10) * sin;
-  const mx = cx + (outerRadius + 30) * cos;
-  const my = cy + (outerRadius + 30) * sin;
-  const ex = mx + (cos >= 0 ? 1 : -1) * 22;
-  const ey = my;
-  const textAnchor = cos >= 0 ? "start" : "end";
+  
+  
 
   return (
     <g>
@@ -48,13 +37,8 @@ const renderActiveShape = (props: any) => {
 };
 
 
-const [activeIndex, setActiveIndex] = useState(0);
-  const onPieEnter = useCallback(
-    (_: any, index: any) => {
-      setActiveIndex(index);
-    },
-    [setActiveIndex]
-  );
+const [activeIndex, ] = useState(0);
+  
 
   return (
     <PieChart width={150} height={150}>
